a4b3bc8b385f72415d1b84c9491b78120a226530
Dan Mergens <danmergens@users.noreply.github.com>
2017-05-26 13:10:12 -0700

    Updated to use authentication
    Security patch to use required authentication - Requires use of auth.sh script to define ELEMENTAL_USER and ELEMENTAL_KEY

--- elemental/record.sh
+++ elemental/record.sh
@@ -3,7 +3,18 @@
 # Start, stop, or restart the video recording for CAMHD on elemental live
 # -------------------------------------------------------------------------
 
+source /data/server/camhd/auth.sh
+AUTH="--login $ELEMENTAL_USER --api-key $ELEMENTAL_KEY"
+
 LOG="/data/server/camhd/control.log"
+ACCEPT="Accept: application/xml"
+CONTENT="Content-type: application/xml"
+GROUP_ID="<group_id>27</group_id>"
+STOP_GROUP="http://209.124.182.238/api/live_events/4/stop_output_group"
+START_GROUP="http://209.124.182.238/api/live_events/4/start_output_group"
+ROLLOVER="http://209.124.182.238/api/live_events/4/rollover_output"
+CURL=/opt/elemental_se/web/public/authentication_scripts/auth_curl.pl
+CURL_AUTH="$CURL $AUTH"
 
 function usage {
         echo "record.sh (--start | --stop | --restart | --rollover | --usage)"
@@ -45,7 +56,7 @@ fi
 
 if [ $do_stop ]; then
 	echo `date` "Info - Stopping recording" >> $LOG
-	curl -X POST -H "Accept: application/xml" -H "Content-type: application/xml" -d "<group_id>27</group_id>" "http://209.124.182.238/api/live_events/4/stop_output_group" 2>&1 >> $LOG
+	$CURL_AUTH -H "$ACCEPT" -H "$CONTENT" -d "$GROUP_ID" $STOP_GROUP 2>&1 >> $LOG
 	if [ $do_start ]; then
 		# give the mp4 recording enough time to close out before starting again
 		sleep 10
@@ -54,13 +65,14 @@ fi
 
 if [ $do_start ]; then
 	echo `date` "Info - Starting recording" >> $LOG
-	curl -X POST -H "Accept: application/xml" -H "Content-type: application/xml" -d "<group_id>27</group_id>" "http://209.124.182.238/api/live_events/4/start_output_group" 2>&1 >> $LOG
+	$CURL_AUTH -H "$ACCEPT" -H "$CONTENT" -d "$GROUP_ID" $START_GROUP 2>&1 >> $LOG
+	# $POST $START
 fi
 
 if [ $do_rollover ]; then
-        echo `date` "Info - Rollover recording" >> $LOG
-        curl -X POST -H "Accept: application/xml" -H "Content-type: application/xml" -d "<output_id>111</output_id>" "http://209.124.182.238/api/live_events/4/rollover_output" 2>&1 >> $LOG
-        curl -X POST -H "Accept: application/xml" -H "Content-type: application/xml" -d "<output_id>112</output_id>" "http://209.124.182.238/api/live_events/4/rollover_output" 2>&1 >> $LOG
+	echo `date` "Info - Rollover recording" >> $LOG
+	$CURL_AUTH -H "$ACCEPT" -H "$CONTENT" -d "<output_id>111</output_id>" $ROLLOVER 2>&1 >> $LOG
+	$CURL_AUTH -H "$ACCEPT" -H "$CONTENT" -d "<output_id>112</output_id>" $ROLLOVER 2>&1 >> $LOG
 fi
 
 exit 0
